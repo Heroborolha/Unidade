b9743ebcfe418622ec4129f2bacd29ff9f36c272
Heroborolha <andrejordan81@yahoo.com>
2023-06-24 16:54:29 -0300

    codigo do movimento do drone atualizado, onde ele nao vai ate a borda da imagem encontrada

--- codigo Airsim/rec_airsim.py
+++ codigo Airsim/rec_airsim.py
@@ -1,19 +1,22 @@
-import airsim #pip install airsim
+import airsim
 import cv2
 import numpy as np
-import time
 
 client = airsim.MultirotorClient()
 client.confirmConnection()
 client.enableApiControl(True)
 client.armDisarm(True)
 client.takeoffAsync().join()
+client.takeoffAsync().join()
+client.takeoffAsync().join()
+client.takeoffAsync().join()
+client.takeoffAsync().join()
+client.takeoffAsync().join()
+client.takeoffAsync().join()
+client.takeoffAsync().join()
 
 def transform_input(responses):
     img1d = np.frombuffer(responses.image_data_uint8, dtype=np.uint8)
-
-    #img1d = 255 / np.maximum(np.ones(img1d.size), img1d)
-
     img_rgba = img1d.reshape(responses.height, responses.width, 3)
 
     from PIL import Image
@@ -24,24 +27,8 @@ def transform_input(responses):
 
     return im_final
 
-"""def index_of_biggest_object(object_list):
-    WIdthPlusHeight = []
-    for i in range(len(object_list)):
-        x, y, w, h = object_list[i]
-        WIdthPlusHeight.append(w+h)
-    max_value = max(WIdthPlusHeight)
-    return  WIdthPlusHeight.index(max_value)
-
-def position_of_object(biggest_object):
-    x, y, w, h = biggest_object
-    x_box = (x+w)/2
-    if x_box < 200: # 받아오는 이미지의 크기를 몰라서 임의로 설정한 값
-        return "left"
-    else:
-        return "right"
-"""
 # Load Yolo
-net = cv2.dnn.readNetFromDarknet("yolov4-custom.cfg", "yolov4.weights")
+net = cv2.dnn.readNetFromDarknet("yolov4-custom.cfg", "yolov5.weights")
 classes = []
 with open("obj.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
@@ -51,10 +38,10 @@ colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
 while(True):
     # Loading image
-    img = client.simGetImages([airsim.ImageRequest("0", airsim.ImageType.Scene, False, False)])[0]
+    img = client.simGetImages([airsim.ImageRequest("3", airsim.ImageType.Scene, False, False)])[0]
     img = transform_input(img)
     img = cv2.resize(img, None, fx=3, fy=3)
-    height, width, channels = img.shape
+    altura, largura, channels = img.shape
 
     # Detecting objects
     blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
@@ -72,41 +59,38 @@ while(True):
             confidence = scores[class_id]
             if confidence > 0.5:
                 # Object detected
-                center_x = int(detection[0] * width)
-                center_y = int(detection[1] * height)
-                w = int(detection[2] * width)
-                h = int(detection[3] * height)
+                centro_x = int(detection[0] * largura)
+                centro_y = int(detection[1] * altura)
+                w = int(detection[2] * largura)
+                h = int(detection[3] * altura)
                 # Rectangle coordinates
-                x = int(center_x - w / 2)
-                y = int(center_y - h / 2)
+                x = int(centro_x - w / 2)
+                y = int(centro_y - h / 2)
                 boxes.append([x, y, w, h])
                 confidences.append(float(confidence))
                 class_ids.append(class_id)
 
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
 
-    font = cv2.FONT_HERSHEY_PLAIN
+    fonte = cv2.FONT_HERSHEY_PLAIN
 
     ObjectWidthPlusHeight = []
     for i in range(len(boxes)):
-        if i in indexes:
+        if i in indices:
             x, y, w, h = boxes[i]
             label = str(classes[class_ids[i]])
-            #color = colors[i]
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(img, label, (x, y + 30), font, 1, (0, 255, 0), 3)
-            object_center_x = center_x
-            object_center_y = center_y
-            image_center_x = img.width //2
-            image_center_y = img.height //2
-            delta_x = object_center_x - image_center_x
-            delta_y = object_center_y - image_center_y
-            max_speed = 1.0
-            speed_x = max_speed * (delta_x / image_center_x)
-            speed_y = max_speed * (delta_y / image_center_y)
+            cv2.putText(img, label, (x, y + 30), fonte, 1, (0, 255, 0), 3)
+            imagem_centro = largura / 2
+            desvio = centro_x - imagem_centro
+            voar = desvio / imagem_centro
+            velocidade = 5
+            ganho_de_voo = 0.5
+            yaw_rate = ganho_de_voo * voar
+            client.rotateByYawRateAsync(yaw_rate=yaw_rate, duration=0.1)
+            client.moveByVelocityAsync(velocidade, 0, 0, duration=0.1)
+            
 
-            client.moveByVelocity(speed_x, speed_y, 0, 1)
-    #png_image = cv2.resize(png_image, dsize=None, fx=3, fy=3)
     cv2.imshow('img',img)
     key = cv2.waitKey(1)
 
